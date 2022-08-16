7b5be3a64f5fb5734254c4fd2864d1bbab3f2c1d
Andrii Voloshchenko <post4tomcat@gmail.com>
2022-08-17 01:26:01 +0300

    change base for gh pages purpose

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    devSourcemap: true
-  },
+    base: 'https://photocat.github.io/dummy-posts',
+    plugins: [react()],
+    css: {
+        devSourcemap: true
+    },
 })
